7b3d2025f4c69a70b27e1d7ddab50423b03a6712
Raffael Eccard <69486695+Losted25@users.noreply.github.com>
2024-07-03 12:35:42 +0000

    Endpoint Read All

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+// Desafio: criar endpoint /oi que exibe "Olá, mundo!"
+app.get('/oi', function (req, res) {
+  res.send('Olá, mundo!')
+})
 
-//Desafio proposto
-app.get('/oi', function (req, res){
-  res.send("Olá, mundo!!")
+// Lista de Personagens
+const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+
+// Read All - [GET] /item
+app.get('/item', function (req, res) {
+  // Pegamos a lista e enviamos como resposta HTTP
+  res.send(lista)
 })
 
 app.listen(3000)
